206321f9874c2b17669d4fcd7e09abb0605182b2
gregoriB <brandon.gregori@gmail.com>
2025-02-21 17:59:50 -0500

    Fix screen flash on load, update default theme to be dark theme

--- scripts/theme_toggle.js
+++ scripts/theme_toggle.js
@@ -4,19 +4,24 @@ const darkCodetheme = "atom-one-dark";
 const toggleSwitch = document.querySelector(
   '.theme-switch input[type="checkbox"]',
 );
-const currentTheme = localStorage.getItem("theme");
 
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
+function setCurrentTheme() {
+  const currentTheme = localStorage.getItem("theme");
 
-  if (currentTheme === "dark") {
-    changeTheme(darkCodetheme);
-    toggleSwitch.checked = true;
+  if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+
+    if (currentTheme === "light") {
+      toggleSwitch.checked = true;
+      changeTheme(lightCodetheme);
+    } else {
+      changeTheme(darkCodetheme);
+    }
   }
 }
 
 function switchTheme(e) {
-  if (e.target.checked) {
+  if (!e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "dark");
     changeTheme(darkCodetheme);
@@ -28,3 +33,5 @@ function switchTheme(e) {
 }
 
 toggleSwitch.addEventListener("change", switchTheme, false);
+
+setCurrentTheme();
